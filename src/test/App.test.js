5f572633a02bde0e8715ea41924ac45911d3c60e
s tech <email@sarcas.tech>
2018-08-07 13:56:14 -0400

    basic usage w/ jest & enzyme

---
+++ src/test/App.test.js
@@ -0,0 +1,15 @@
+/* global describe, it, expect */
+import React from 'react'
+// import ReactDOM from 'react-dom'
+import {configure, shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from '../App'
+
+configure({ adapter: new Adapter() })
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    let app = shallow(<App />)
+    expect(app.find('.App')).toHaveLength(1)
+  })
+})
