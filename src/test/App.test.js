e477e2b80547d7e64bcfb283f71b7b69b41f5b5f
s tech <email@sarcas.tech>
2018-08-07 14:26:43 -0400

    update js files to be lint compliant

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,6 +1,5 @@
 /* global describe, it, expect */
 import React from 'react'
-// import ReactDOM from 'react-dom'
 import {configure, shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import App from '../App'
