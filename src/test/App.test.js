aac631c5b4929c5bfc19303e72bee7085ec58139
s tech <email@sarcas.tech>
2018-08-07 14:41:17 -0400

    remove flag from testing and update app test file with more checks

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -7,8 +7,16 @@ import App from '../App'
 configure({ adapter: new Adapter() })
 
 describe('<App />', () => {
-  it('renders without crashing', () => {
-    let app = shallow(<App />)
+  let app = shallow(<App />)
+  it('renders with correct css class assigned', () => {
     expect(app.find('.App')).toHaveLength(1)
+  })
+
+  it('renders correct welcome message', () => {
+    expect(app.find('.App-title').text()).toBe('Welcome to React')
+  })
+
+  it('contains a `getting started` paragraph', () => {
+    expect(app.find('p.App-intro').text()).toContain('To get started,')
   })
 })
