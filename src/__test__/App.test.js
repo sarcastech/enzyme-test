37173c2cefe346bec6beb436b0ea516e2395227c
s tech <email@sarcas.tech>
2018-08-08 12:21:39 -0400

    add test to confirm <DataComponent /> is within app.js

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -4,6 +4,7 @@ import {configure, shallow} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import App from '../App'
 import Foo from '../Foo'
+import DataComponent from '../DataComponent'
 
 configure({ adapter: new Adapter() })
 
@@ -21,7 +22,11 @@ describe('<App />', () => {
     expect(app.find('p').text()).toContain('To get started,')
   })
 
-  it('wraps 2 Foo components', () => {
+  it('wraps 2 <Foo /> components', () => {
     expect(app.find(Foo)).toHaveLength(2)
+  })
+
+  it('wraps a <DataComponent />', () => {
+    expect(app.find(DataComponent)).toHaveLength(1)
   })
 })
